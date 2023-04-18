e98be9a077f2b576e9866b63a37281a31817497a
Ethan Wu <ew2664@columbia.edu>
2023-04-17 22:38:59 -0400

    Update signup

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: "us-east-1_udaN1zRTa",
-    ClientId: "6m6gc2b93e9d0lnceqg9i3jk2o"
-}
+  UserPoolId: "us-east-1_XdqZBOfji",
+  ClientId: "4km322kn7codmd495hvt8p76t0",
+};
 
 export default new CognitoUserPool(poolData);
