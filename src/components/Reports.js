ad9086f8fdee23ff9f65c9a665ff106ea803be4f
Ethan Wu <ew2664@columbia.edu>
2023-04-17 23:10:38 -0400

    Add API calls to reports page

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,86 +1,80 @@
-import React, { useState } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import { Container } from '@mui/material';
+import React, { useState, useContext, useEffect } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Container } from "@mui/material";
 import { Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
-import { useNavigate } from "react-router";
-import Report from './Report';
+import { AccountContext } from "./AccountContext";
+import apigClient from "../ApigClient";
 
 const Reports = () => {
+  const { session } = useContext(AccountContext);
 
-  let navigate = useNavigate();
+  const [reports, setReports] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const handleClick = (event, cellValues) => {
-    console.log(cellValues.row);
+  const getReports = async () => {
+    try {
+      const response = await apigClient.invokeApi({}, "/admin/reports", "GET", {
+        headers: { Authorization: session["idToken"]["jwtToken"] },
+      });
+      console.log(response);
+      setReports(
+        response.data.map((report) => ({ ...report, id: report.reportID }))
+      );
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
-  
+
+  useEffect(() => {
+    getReports();
+  }, []);
+
   const columns = [
-    { field: 'id', headerName: 'Report #', type: 'number', width: 70 },
-    { field: 'reportTitle', headerName: 'Report Title', width: 300 },
-    { field: 'description', headerName: 'Description' },
-    { field: 'location', headerName: 'Location', width: 200 },
-    { field: 'status', headerName: 'Status', width: 200 },
-    // { field: 'details', headerName: 'Details', sortable: false, width: 150 }
+    { field: "reportID", headerName: "Report #", type: "number", width: 70 },
+    { field: "title", headerName: "Report Title", width: 300 },
+    { field: "description", headerName: "Description" },
+    { field: "location", headerName: "Location", width: 200 },
+    { field: "status", headerName: "Status", width: 200 },
     {
-      field: 'details',
-      headerName: 'Details',
+      field: "details",
+      headerName: "Details",
       sortable: false,
       renderCell: (cellValues) => {
         return (
-          <Link to={"/report/" + cellValues.id} state={{ data: cellValues.row }}>
+          <Link
+            to={"/report/" + cellValues.id}
+            state={{ data: cellValues.row }}
+          >
             Details
           </Link>
         );
       },
     },
-    { field: 'date' },
-    // {
-    //   field: 'fullName',
-    //   headerName: 'Full name',
-    //   description: 'This column has a value getter and is not sortable.',
-    //   sortable: false,
-    //   width: 160,
-    //   valueGetter: (params) =>
-    //     `${params.row.id || ''} ${params.row || ''}`,
-    // },
-  ];
-  
-  const rows = [
-    { id: 1, reportTitle: 'Clogged sink in 2016 kitchen', description: "The sink is clogged and has been clogged since last Sunday.", location: 'East Campus', status: 'Submitted', date: "1/20/2023 2:58 PM EST" },
-    { id: 3, reportTitle: 'Broken radiator in 2020A', description: "Radiator keeps on making weird noises. It will keep turning on and off for 5 minutes straight every day.", location: 'East Campus', status: 'In progress', date: "2/20/2023 1:00 PM EST" },
-    { id: 6, reportTitle: 'Air conditioner not working in 627', description: "AC doesn't work.", location: 'Mudd Hall', status: 'In progress', date: "12/2/2022 11:31 AM EST" },
+    { field: "date" },
   ];
 
-  // let tempRows = rows;
-
-  // let selected = [];
-
-  // for (const [key, val] of Object.entries(tempRows)) {
-  //   console.log(val);
-  //   delete val.description;
-  //   delete val.date;
-  //   selected.push(val);
-  // }
-
-  return (
+  return loading ? (
+    ""
+  ) : (
     <Container maxWidth="lg">
-        <div style={{ fontSize: '200%' }}>Reports</div>
-        <div style={{ height: 600, width: '100%' }}>
+      <div style={{ fontSize: "200%" }}>Reports</div>
+      <div style={{ height: 600, width: "100%" }}>
         <DataGrid
-            initialState={{
-              columns: {
-                columnVisibilityModel: {
-                  description: false,
-                  date: false,
-                },
+          initialState={{
+            columns: {
+              columnVisibilityModel: {
+                description: false,
+                date: false,
               },
-            }}
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
+            },
+          }}
+          rows={reports}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
         />
-        </div>
+      </div>
     </Container>
   );
 };
