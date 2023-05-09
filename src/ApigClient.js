986b0771e68896d9a33dfd607840036953162ce2
Ethan Wu <ew2664@columbia.edu>
2023-05-08 20:34:24 -0400

    Update API invoke URL

--- src/ApigClient.js
+++ src/ApigClient.js
@@ -1,6 +1,6 @@
 var apigClientFactory = require("aws-api-gateway-client").default;
 var apigClient = apigClientFactory.newClient({
-  invokeUrl: "https://1fbyvh4q99.execute-api.us-east-1.amazonaws.com/dev",
+  invokeUrl: "https://iumszzd0mi.execute-api.us-east-1.amazonaws.com/dev",
 });
 
 export default apigClient;
